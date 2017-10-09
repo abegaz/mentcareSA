82ea65bb9a78c79be0d210edef142d5ff40f994a
Joshua Knight <101josh10@gmail.com>
2017-10-09 17:02:11 -0400

    updated info for Database design

--- MentCareSA/src/application/Main.java
+++ MentCareSA/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,8 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/PhysicianMainView.fxml"));
+			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
---
+++ MentCareSA/src/com/mentCare/controller/PhysicianMainController.java
@@ -0,0 +1,5 @@
+package com.mentCare.controller;
+
+public class PhysicianMainController {
+
+}
