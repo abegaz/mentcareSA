a31d4d00e55b133ab45ad2bc18afd815d4485050
Joshua Knight <101josh10@gmail.com>
2017-10-31 10:55:21 -0400

    Linked objects from View to Controller

--- MentCareSA/src/com/mentCare/controller/PhysicianMainController.java
+++ MentCareSA/src/com/mentCare/controller/PhysicianMainController.java
@@ -1,5 +1,35 @@
 package com.mentCare.controller;
 
-public class PhysicianMainController {
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+public class PhysicianMainController {
+	//configure the Patient Table
+	@FXML private TableView patientTableView;
+	@FXML private TableColumn patientTableColumn;
+	
+	//configure Patient Fields
+	@FXML private TextField nameField;
+	@FXML private DatePicker dobPicker;
+	@FXML private ComboBox genderCombo;
+	@FXML private TextField ssnField;
+	@FXML private ComboBox bloodTypeCombo;
+	@FXML private TextField phoneNumField;
+	@FXML private TextField emailField;
+	@FXML private TextField emerNameField;
+	@FXML private TextField emerPhoneField;
+	@FXML private TextField emerEmailField;
+	@FXML private TextField emerRelationField;
+	@FXML private TextArea conditionArea;
+	@FXML private TextArea treatmentArea;
+	@FXML private TextArea notesArea;
+	
+	public void initialize() {
+		
+	}
 }
