587af328827ea6fedeb4755cad00fbd6900f75c7
Joshua Knight <101josh10@gmail.com>
2017-11-02 15:35:05 -0400

    Update Patient.java

--- MentCareSA/src/com/mentCare/model/Patient.java
+++ MentCareSA/src/com/mentCare/model/Patient.java
@@ -1,28 +1,81 @@
 package com.mentCare.model;
 
-public class EmergencyContact {
-	private String lastName, firstName, phoneNum, email;
-
-	public EmergencyContact() {
+public class Patient {
+	//Declare variables
+	private String lastName, middleName, firstName;
+	private String email;
+	private String password;
+	private String ssn;
+	private String bloodType;
+	private String phoneNum;
+	private Address address;
+	private String height, weight;
+	private boolean organDonor;
+	private EmergencyContact emerContact;
+	private String condition, treatment, notes;
+	
+	//******************Constructors******************************
+	public Patient() {
+		super();
 		lastName = "";
+		middleName = "";
 		firstName = "";
-		phoneNum = "";
 		email = "";
+		password = "";
+		ssn = "";
+		bloodType = "";
+		phoneNum = "";
+		address = new Address();
+		height = "";
+		weight = "";
+		organDonor = false;
+		emerContact = new EmergencyContact();
+		condition = "";
+		treatment = "";
+		notes = "";
 	}
 	
-	public EmergencyContact(String lastName, String firstName, String phoneNum, String email) {
-		super();
+	public Patient(String lastName, String middleName, String firstName, String email, String password, String ssn,
+			String bloodType, String phoneNum, Address address, String height, String weight, boolean organDonor,
+			EmergencyContact emerContact, String condition, String treatment, String notes) {
 		this.lastName = lastName;
+		this.middleName = middleName;
 		this.firstName = firstName;
-		this.phoneNum = phoneNum;
 		this.email = email;
+		this.password = password;
+		this.ssn = ssn;
+		this.bloodType = bloodType;
+		this.phoneNum = phoneNum;
+		this.address = address;
+		this.height = height;
+		this.weight = weight;
+		this.organDonor = organDonor;
+		this.emerContact = emerContact;
+		this.condition = condition;
+		this.treatment = treatment;
+		this.notes = notes;
 	}
 
+	public Patient(String lastName, String firstName, String middleName) {
+		this.lastName = lastName;
+		this.middleName = middleName;
+		this.firstName = firstName;
+	}
+
+//*********************Getters*********************	
+	
 	/**
 	 * @return the lastName
 	 */
 	public String getLastName() {
 		return lastName;
+	}
+
+	/**
+	 * @return the middleName
+	 */
+	public String getMiddleName() {
+		return middleName;
 	}
 
 	/**
@@ -33,13 +86,6 @@ public class EmergencyContact {
 	}
 
 	/**
-	 * @return the phoneNum
-	 */
-	public String getPhoneNum() {
-		return phoneNum;
-	}
-
-	/**
 	 * @return the email
 	 */
 	public String getEmail() {
@@ -47,10 +93,103 @@ public class EmergencyContact {
 	}
 
 	/**
+	 * @return the ssn
+	 */
+	public String getSsn() {
+		return ssn;
+	}
+
+	/**
+	 * @return the bloodType
+	 */
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	/**
+	 * @return the phoneNum
+	 */
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public String getHeight() {
+		return height;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public String getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @return the organDonor
+	 */
+	public boolean isOrganDonor() {
+		return organDonor;
+	}
+
+	/**
+	 * @return the condition
+	 */
+	public String getCondition() {
+		return condition;
+	}
+
+	/**
+	 * @return the treatment
+	 */
+	public String getTreatment() {
+		return treatment;
+	}
+
+	/**
+	 * @return the notes
+	 */
+	public String getNotes() {
+		return notes;
+	}
+	
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+
+	/**
+	 * @return the emerContact
+	 */
+	public EmergencyContact getEmerContact() {
+		return emerContact;
+	}
+
+	//*********************Setters*********************
+	
+	/**
 	 * @param lastName the lastName to set
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	/**
+	 * @param middleName the middleName to set
+	 */
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
 	}
 
 	/**
@@ -61,6 +200,27 @@ public class EmergencyContact {
 	}
 
 	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @param ssn the ssn to set
+	 */
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	/**
+	 * @param bloodType the bloodType to set
+	 */
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
+
+	/**
 	 * @param phoneNum the phoneNum to set
 	 */
 	public void setPhoneNum(String phoneNum) {
@@ -68,22 +228,90 @@ public class EmergencyContact {
 	}
 
 	/**
-	 * @param email the email to set
+	 * @param height the height to set
 	 */
-	public void setEmail(String email) {
-		this.email = email;
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * @param organDonor the organDonor to set
+	 */
+	public void setOrganDonor(boolean organDonor) {
+		this.organDonor = organDonor;
+	}
+
+	/**
+	 * @param condition the condition to set
+	 */
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	/**
+	 * @param treatment the treatment to set
+	 */
+	public void setTreatment(String treatment) {
+		this.treatment = treatment;
+	}
+
+	/**
+	 * @param notes the notes to set
+	 */
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	/**
+	 * @param emerContact the emerContact to set
+	 */
+	public void setEmerContact(EmergencyContact emerContact) {
+		this.emerContact = emerContact;
 	}
 	
 	/*equals
-	 * used to compare 2 contacts to each other. Returns true if they are the exact same, false otherwise.
+	 * used to compare 2 patients to each other. Returns true if they are the exact same, false otherwise.
 	 */
-	public boolean equals(EmergencyContact newContact) {
+
+	public boolean equals(Patient newPatient) {
 		boolean theSame = false;
 		
-		if(firstName.equals(newContact.getFirstName()) &&
-		   lastName.equals(newContact.getLastName()) &&
-		   phoneNum.equals(newContact.getPhoneNum()) &&
-		   email.equals(newContact.getEmail())) {
+		if(lastName.equals(newPatient.getLastName()) &&
+		   middleName.equals(newPatient.getMiddleName()) &&
+		   lastName.equals(newPatient.getLastName()) &&
+		   address.equals(newPatient.getAddress()) &&
+		   bloodType.equals(newPatient.getBloodType()) &&
+		   condition.equals(newPatient.getCondition()) &&
+		   email.equals(newPatient.getEmail()) && 
+		   emerContact.equals(newPatient.getEmerContact()) &&
+		   height.equals(newPatient.getHeight()) &&
+		   notes.equals(newPatient.getNotes()) &&
+		   isOrganDonor() == newPatient.isOrganDonor() &&
+		   phoneNum.equals(newPatient.getPhoneNum()) &&
+		   ssn.equals(newPatient.getSsn()) &&
+		   treatment.equals(newPatient.getTreatment()) &&
+		   weight.equals(newPatient.getWeight())) {
 			theSame = true;
 		}
 		
@@ -95,17 +323,37 @@ public class EmergencyContact {
 	 * "A".equalsIgnoreCase("a"); will return true
 	 */
 	
-	public boolean equalsIgnoreCase(EmergencyContact newContact) {
+	public boolean equalsIgnoreCase(Patient newPatient) {
 		boolean theSame = false;
 		
-		if(firstName.equalsIgnoreCase(newContact.getFirstName()) &&
-		   lastName.equalsIgnoreCase(newContact.getLastName()) &&
-		   phoneNum.equalsIgnoreCase(newContact.getPhoneNum()) &&
-		   email.equalsIgnoreCase(newContact.getEmail())) {
+		if(lastName.equalsIgnoreCase(newPatient.getLastName()) &&
+		   middleName.equalsIgnoreCase(newPatient.getMiddleName()) &&
+		   lastName.equalsIgnoreCase(newPatient.getLastName()) &&
+		   address.equalsIgnoreCase(newPatient.getAddress()) &&
+		   bloodType.equalsIgnoreCase(newPatient.getBloodType()) &&
+		   condition.equalsIgnoreCase(newPatient.getCondition()) &&
+		   email.equalsIgnoreCase(newPatient.getEmail()) && 
+		   emerContact.equalsIgnoreCase(newPatient.getEmerContact()) &&
+		   height.equalsIgnoreCase(newPatient.getHeight()) &&
+		   notes.equalsIgnoreCase(newPatient.getNotes()) &&
+		   isOrganDonor() == newPatient.isOrganDonor() &&
+		   phoneNum.equalsIgnoreCase(newPatient.getPhoneNum()) &&
+		   ssn.equalsIgnoreCase(newPatient.getSsn()) &&
+		   treatment.equalsIgnoreCase(newPatient.getTreatment()) &&
+		   weight.equalsIgnoreCase(newPatient.getWeight())) {
 			theSame = true;
 		}
 		
 		return theSame;
 	}
+
+	public String toString() {
+		return "Patient [lastName=" + lastName + ", MiddleName=" + middleName + ", firstName=" + firstName + ", email="
+				+ email + ", password=" + password + ", ssn=" + ssn + ", bloodType=" + bloodType + ", phoneNum="
+				+ phoneNum + ", address=" + address + ", height=" + height + ", weight=" + weight + ", organDonor="
+				+ organDonor + ", emerContact=" + emerContact + ", condition=" + condition + ", treatment=" + treatment
+				+ ", notes=" + notes + "]";
+	}
+	
 	
 }
