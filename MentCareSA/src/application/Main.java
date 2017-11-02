d643a9cf2c50a71e22ae879b5dee255ee385b517
Joshua Knight <101josh10@gmail.com>
2017-11-02 12:09:07 -0400

    Update Main.java

--- MentCareSA/src/application/Main.java
+++ MentCareSA/src/application/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/PhysicianMainView.fxml"));
-			Scene scene = new Scene(root,800,600);
+			Scene scene = new Scene(root,800,708);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
