6f014fec551ef70143d5c941927523d5d53f6495
LeviHarbin <levitheninja@yahoo.com>
2017-11-10 19:31:22 -0500

    updated to start at login

--- MentCareSA/src/application/Main.java
+++ MentCareSA/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/PhysicianMainView.fxml"));
+			Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/login.fxml"));
 			Scene scene = new Scene(root,800,708);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
