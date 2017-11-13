c6c2aa454f9a83868b743562107f98c230bd4c3a
Joshua Knight <101josh10@gmail.com>
2017-11-13 10:19:33 -0500

    Added ability to quickly load specific view for testing purposes

--- MentCareSA/src/application/Main.java
+++ MentCareSA/src/application/Main.java
@@ -1,3 +1,15 @@
+/*Class Name: Main
+ * 
+ * The Purpose of this class is to set up the window for the application, as well as be the
+ * main controller for the application as a whole
+ * 
+ * Notes:
+ * For testing purposes, there are 3 statements, each that assign root to a different view
+ * to test your view, uncomment your view and comment the other two.
+ * 
+ * To run the application as a whole, uncomment login (the fist root statement)
+ */
+
 package application;
 	
 import javafx.application.Application;
@@ -13,6 +25,8 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/login.fxml"));
+			//Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/PhysicianMainView.fxml"));
+			//Parent  root = FXMLLoader.load(getClass().getResource("../com/mentCare/view/PatientMainView.fxml"));
 			Scene scene = new Scene(root,800,708);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
