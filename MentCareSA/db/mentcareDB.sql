101df4c1d794c99fa755b1698dc3ab34ec7f24c1
Garrett Gruber <31317817+GarrettTooCool@users.noreply.github.com>
2017-11-29 13:36:26 -0500

    Added Celia's insert commands

--- MentCareSA/db/mentcareDB.sql
+++ MentCareSA/db/mentcareDB.sql
@@ -7,6 +7,8 @@ CREATE TABLE LOGIN (
     Psswrd VARCHAR(20),
     PRIMARY KEY (Username)
 );
+INSERT INTO LOGIN (Username, Psswrd)
+VALUES ('123456','doctor1'),('213452','doctor2'),('234451','doctor3'),('213000','doctor4');
 
 CREATE TABLE PAT_EMERGENCY (
     Em_Id VARCHAR(6),
@@ -114,6 +116,8 @@ CREATE TABLE PHYSICIAN_INFO (
     Phys_phone VARCHAR(15),
     PRIMARY KEY (Phys_id)
 );
+INSERT INTO PHYSICIAN_INFO (Phys_id, Phys_name, Phys_email, Phys_phone)
+VALUES ('123456', 'Dr. John Smith', 'drjohnsmith@email.com', '5552223333'),('213452', 'Dr. Jane Black', 'drjaneblack@email.com', '7776668888'),('234451', 'Dr. Doug Dunbar', 'drdougdunbar@email.com', '2223334444'),('213000', 'Dr. Rebecca Regan', 'drrebeccaregan@email.com', '1119998888');
 
 CREATE TABLE MED_FACILITY (
     Fac_id VARCHAR(6),
@@ -122,6 +126,8 @@ CREATE TABLE MED_FACILITY (
     Fac_phone VARCHAR(15),
     PRIMARY KEY (Fac_id)
 );
+INSERT INTO MED_FACILITY (Fac_id, Fac_address, Fac_name, Fac_phone)
+VALUES ('10', '123 Main St', 'Braircliff Hospital', '5555555555'), ('11', '432 Maple St', 'Greenlawn Hospital', '2222222222'), ('12', '561 Clay Road', 'GoldValley Hospital', '1111111111');
 
 CREATE TABLE WORKS_AT (
     Phys_id VARCHAR(6),
@@ -135,3 +141,5 @@ CREATE TABLE WORKS_AT (
     FOREIGN KEY (Fac_id)
         REFERENCES MED_FACILITY (Fac_id)
 );
+INSERT INTO WORKS_AT (Phys_id, Fac_id, Off_phone, Room_no)
+VALUES ('123456', '10', '5552224444', '255'),('213452', '10', '5551112222', '264'),('234451', '11', '2221114343', '104'),('213000', '12', '1112324444', '301');
